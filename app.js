a002fac2456a7d2c7ee4835e558aad479d793dc0
Evgeniy Shakhura <shah5@yandex.ru>
2022-09-11 14:49:51 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const { routes } = require('./routes');
 const { requestLogger, errorLoger } = require('./middlewares/logger');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { apiLimiter } = require('./middlewares/apiLimiter');
+const { cors } = require('./middlewares/cors');
 
 const { DATABASE_ADDRESS = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors);
 app.use(helmet());
 mongoose.connect(DATABASE_ADDRESS, {
   useNewUrlParser: true,
